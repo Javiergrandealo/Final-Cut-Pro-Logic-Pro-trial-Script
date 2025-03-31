ea94d262594ff23c9db391dce8e0aa4a25397450
Javier Grande Alonso <110691482+Javiergrandealo@users.noreply.github.com>
2025-03-31 12:05:27 +0200

    Update Script_LogicPro_y_Final_Cut.sh

--- Script_LogicPro_y_Final_Cut.sh
+++ Script_LogicPro_y_Final_Cut.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-# Mueve los archivos ocultos a la Papelera
-mv -v /Users/javiergrande/Library/Application\ Support/.lpxuserdata ~/.Trash
+# Move hidden files to trash
+mv -v /Users/"your user here without quotes"/Library/Application\ Support/.lpxuserdata ~/.Trash
 mv -v ~/Library/Containers/com.apple.FinalCutTrial/Data/Library/Application\ Support/.ffuserdata ~/.Trash
