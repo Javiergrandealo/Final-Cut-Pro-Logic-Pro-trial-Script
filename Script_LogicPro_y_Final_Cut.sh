e815bd6948dae5b9f832cfa23b95cd519e5a9cc6
Javier Grande Alonso <110691482+Javiergrandealo@users.noreply.github.com>
2025-03-31 12:04:07 +0200

    Upload Script

---
+++ Script_LogicPro_y_Final_Cut.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+# Mueve los archivos ocultos a la Papelera
+mv -v /Users/javiergrande/Library/Application\ Support/.lpxuserdata ~/.Trash
+mv -v ~/Library/Containers/com.apple.FinalCutTrial/Data/Library/Application\ Support/.ffuserdata ~/.Trash
